cfb0404b902945daca63478115e752af6e9d49bf
Wyatt-Destabelle <106564974+Wyatt-Destabelle@users.noreply.github.com>
2024-11-09 21:28:32 -0500

    Small Script Edits

--- BigtownThrowdown/Assets/Scripts/PersonScript.cs
+++ BigtownThrowdown/Assets/Scripts/PersonScript.cs
@@ -9,6 +9,8 @@ public class PersonScript : MonoBehaviour
     float directionTime;
     public float minTime,maxTime;
     float speed = 2;
+    enum PersonState{Wander,Flee,Build,Chat}
+    PersonState currentState;
     // Start is called before the first frame update
     void Start()
     {
